3462025db19648019a2713b66aeda17f5c2bf2d0
Denis <you@example.com>
2019-09-25 21:59:38 +0300

    Version Operators

---
+++ OOP_Lab_3/OOP_Lab_3/MathOperation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_3
+{
+    static class MathOperation
+    {
+        public static int GetMax(Stack<int> stack)
+        {
+            if (stack.isEmpty())
+            {
+                return -1;
+            }
+            Stack<int> Math = (Stack<int>)stack.Clone();
+            int[] arr = new int[Math.size];
+            int max = 0;
+            for (int i = 0; !Math.isEmpty(); i++)
+            {
+                arr[i] = Math.top();
+                if (arr[i]>arr[max])
+                {
+                    max = i;
+                }
+                Math.pop();
+            }
+            return arr[max];
+        }
+
+        public static int GetMin(Stack<int> stack)
+        {
+            if (stack.isEmpty())
+            {
+                return -1;
+            }
+            Stack<int> Math = (Stack<int>)stack.Clone();
+            int[] arr = new int[Math.size];
+            int min = 0;
+            for (int i = 0; !Math.isEmpty(); i++)
+            {
+                arr[i] = Math.top();
+                if (arr[i] < arr[min])
+                {
+                    min = i;
+                }
+                Math.pop();
+            }
+            return arr[min];
+        }
+
+        public static uint Length(Stack<int> stack)
+        {
+            return stack.size;
+        }
+    }
+}
---
+++ OOP_Lab_3/OOP_Lab_3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> MyStack_1 = new Stack<int>();
+            Stack<int> MyStack_2 = new Stack<int>();
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                MyStack_1.push(rand.Next(1, 100));
+                MyStack_2.push(rand.Next(1, 100));
+            }
+
+            Stack<int>.Show(MyStack_1);
+            Stack<int>.Show(MyStack_2);
+
+            Console.WriteLine();
+
+            MyStack_1 = MyStack_1 < MyStack_2;
+
+            Stack<int>.Show(MyStack_1);
+            Stack<int>.Show(MyStack_2);
+
+            Console.WriteLine();
+
+            for (int i = 0; i < MyStack_2.size; i++)
+            {
+                MyStack_1--;
+            }
+
+            Stack<int>.Show(MyStack_1);
+            Stack<int>.Show(MyStack_2);
+
+            Console.WriteLine();
+
+            MyStack_1 = MyStack_1 + MyStack_2;
+
+            Stack<int>.Show(MyStack_1);
+            Stack<int>.Show(MyStack_2);
+
+            Console.WriteLine();
+
+            MyStack_1.Information();
+            MyStack_2.Information();
+
+            Console.WriteLine(); 
+
+            Console.WriteLine(MathOperation.GetMax(MyStack_1));
+            Console.WriteLine(MathOperation.GetMin(MyStack_1));
+            Console.WriteLine(MathOperation.Length(MyStack_1));
+            Console.WriteLine();
+            Console.WriteLine(MathOperation.GetMax(MyStack_2));
+            Console.WriteLine(MathOperation.GetMin(MyStack_2));
+            Console.WriteLine(MathOperation.Length(MyStack_2));
+        }
+    }
+}
