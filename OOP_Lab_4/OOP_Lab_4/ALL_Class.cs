491e369e9177e43fa09f4528190fcbbc7009118e
Denis <you@example.com>
2019-09-27 09:05:56 +0300

    Complited task

---
+++ OOP_Lab_4/OOP_Lab_4/ALL_Class.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_4
+{
+    public abstract class Earth
+    {
+        public string name;
+        public double Square;
+        public Earth(string str, double num)
+        {
+            name = str;
+            Square = Math.Pow(num, 2);
+        }
+        public override string ToString()
+        {
+            return "Name: " + name + "\nSquare: " + Square + "\n" + base.ToString() + "\n";
+        }
+        public abstract void Show();
+    }
+
+    interface IWater
+    {
+        void Sound();
+        void Show();
+    }
+
+    public class Ocean : IWater
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Wshhh-Wshhh");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("It is a Ocean");
+        }
+    }
+
+    public class Kontinent : Earth
+    {
+        public Kontinent(string name) : base(name, name.Length) { }
+        public override void Show()
+        {
+            Console.WriteLine("It is a Kontinent " + name + "\nSqrt: " + Square);
+        }
+    }
+
+    public class Island : Earth,IWater
+    {
+        public Island(string name) : base(name, name.Length) { }
+        public override void Show()
+        {
+            Console.WriteLine("It is a Island " + name);
+        }
+
+        void IWater.Show()
+        {
+            Console.WriteLine("It is a Island\'s Square: " + Square);
+        }
+
+        public void Sound()
+        {
+            Console.WriteLine("Yooo-Yooo");
+        }
+    }
+
+    sealed class State : Kontinent
+    {
+        public State(string name) : base(name) { }
+        public void Messege()
+        {
+            Console.WriteLine("It is my State " + name);
+        }
+    }
+}
---
+++ OOP_Lab_4/OOP_Lab_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Earth[] array = new Earth[3];
+            Object obj_1 = new Ocean();
+            if (obj_1 is Ocean)
+            {
+                Console.WriteLine("Is Test!");
+                Ocean ocean = (Ocean)obj_1;
+                ocean.Show();
+                ocean.Sound();
+            }
+            Object obj_2 = new Kontinent("Africa");
+            Kontinent land = obj_2 as Kontinent;
+            if (land != null)
+            {
+                array[0] = land;
+                land.Show();
+            }
+            Island island = new Island("Kuba");
+            array[1] = island;
+            island.Show();
+            island.Sound();
+            IWater water = new Island("Kuba");
+            water.Show();
+            ((Island)water).Show();
+            State state = new State("The USA");
+            array[2] = state;
+            state.Show();
+            state.Messege();
+
+            Console.WriteLine();
+
+            foreach (var item in array)
+            {
+                Printer.IAmPrinting(item);
+            }
+        }
+    }
+}
